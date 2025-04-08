f19092185e9d53a7bfd7a2d8b4189d6cdb88a216
GiordanoCasale <giordanocasale8@gmail.com>
2025-04-08 12:25:31 +0200

    [UPD] milestone 2 completed

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
 //vado a recuperare gli elementi dal dom
-const cards= document.getElementById("card")
+const cards = document.getElementById("card")
+const btn = document.getElementById("btn-zoom")
+const card= document.getElementById("cards")
+const overlay= document.getElementById("overlay")
+
 //vado a richiamare l'endpoint
 const endPoint = `https://lanciweb.github.io/demo/api/pictures/`
 //vado a creare la funzione per sostituire le scritte e le foto
-const createCards = (data) =>{
+const createCards = (data) => {
     const newCards = `<div class="col-12 col-md-6 col-lg-4">
                     <div class="card-image bg-light m-5 d-flex justify-content-center flex-direction-column">
                         <div class="card-content mt-4 bg-danger">
@@ -18,11 +22,19 @@ const createCards = (data) =>{
     cards.innerHTML += newCards;
 }
 //chiamata axios per recuperare i dati
-const dati = () =>{
-    axios.get(endPoint).then(response =>{
+const dati = () => {
+    axios.get(endPoint).then(response => {
         response.data.forEach(item => createCards(item))
     });
 };
 
 //vado a richiamare la funzione
 dati();
+//vado a far apparire l'overlay con il click
+cards.addEventListener("click", function(){
+    overlay.classList.remove("d-none")
+})
+//vado a mettere il bottono per la chiusura dell'overlay
+btn.addEventListener("click", function(){
+    overlay.classList.add("d-none")
+})
